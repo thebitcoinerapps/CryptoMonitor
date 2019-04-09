b475d237db64b28d7e4bc2c784984e38c5a1f83d
theBitCoiner <thebitcoinerapps@gmail.com>
2019-04-09 11:08:58 +0200

    css added

--- src/components/column.js
+++ src/components/column.js
@@ -9,24 +9,52 @@ const Column = (props) =>{
                 return img.id === e.id;
             });
         });
-
+        console.log(props.data.data);
         const columns = props.data.data.map((element)=>{
             let pos = element.cmc_rank - 1;
             let price = element.quote.USD.price.toFixed(4);
+
+            let change = element.quote.USD.percent_change_7d;
+            let iconClass = '';
+            let cardClass = '';
+                if(change>0){
+                    iconClass = 'green thumbs up icon';
+                    cardClass = 'ui raised link card';
+                }else{
+                    iconClass = 'red thumbs down icon'
+                    cardClass = "red ui card";
+                }
+
+
             return (
                 <div className="column">
-                    <div className="ui card">
+                    <div className={cardClass}>
                         <div className="content">
+                            <div className="header">
                             {element.name}
-                        <div className="content">
-                            {price}
+                            </div>
+                            <div className="left floated meta">
+                            {price} USD
+                            </div>
+                            </div>
+                            <div className="content">
                             <div className="image">
                             <img src={foundValues[pos].logo}></img>
+                            
                             </div>
-                        </div>
+                            </div>
+                            <div className="extra content">
+                                <div className="ui large transparent left icon input">
+                                <i className={iconClass}></i>
+                                </div>
+                                <div className="right floated meta">
+                                7-day change {change}%
+                                </div>
+                            
+                            </div>
+
                         </div>
                     </div>
-                </div>
             );
         });
     
